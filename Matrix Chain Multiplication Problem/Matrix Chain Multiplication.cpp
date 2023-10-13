e440cd144afdfc3f9932a4bf54761fae707b1f8b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-10-13 13:29:07 +0530

    Create Matrix Chain Multiplication.cpp

---
+++ Matrix Chain Multiplication Problem/Matrix Chain Multiplication.cpp
@@ -0,0 +1,58 @@
+Approach 1 : Recursive 
+
+steps 
+1. find i and j value 
+2. find base condition 
+3. move k -> i to j
+option 1 : k = i to j-1 -> for(i to k) and for(k+1 to j)
+option 2 : k = i+1 to j -> for(i to k-1) and (k to j) 
+
+int solve(int arr[],int i,int j) 
+{
+  if(i>=j) return 0;
+  int mn = INT_MAX;
+  for(int k=i;k<=j-1;k++){
+    int temp_ans = solve(arr,i,k) + solve(arr,k+1,j) + arr[i-1] * arr[k] * arr[j];
+    if(temp_ans < mn) {
+    mn = temp;
+    }
+  }
+  return mn;
+}
+
+Approach 2 : Memoization (Bottom-Up Approach) 
+
+  // User function Template for C++
+
+class Solution{
+    
+public:
+int t[501][501];
+int helper(int arr[],int i,int j)
+{
+    int mn = INT_MAX;
+    //base condition
+    if(i>=j) return 0;
+    if(t[i][j] != -1) return t[i][j];
+//move k 
+for(int k = i; k<=j-1;k++)
+{
+    int temp_ans = helper(arr,i,k) + helper(arr,k+1,j)+
+                    arr[i-1]*arr[k]*arr[j];
+
+    if(temp_ans < mn)
+    {
+        mn =temp_ans;
+    }
+    
+}
+return t[i][j]=mn;
+}
+    int matrixMultiplication(int N, int arr[])
+    {
+        memset(t,-1,sizeof(t));
+   return  helper(arr,1,N-1);
+    }
+};
+
+  
