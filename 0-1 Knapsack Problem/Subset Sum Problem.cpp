960915a6a339bf3b3de366214a3077596518d993
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-10-21 12:38:44 +0530

    Update Subset Sum Problem.cpp

--- 0-1 Knapsack Problem/Subset Sum Problem.cpp
+++ 0-1 Knapsack Problem/Subset Sum Problem.cpp
@@ -1,67 +1,45 @@
-Brute Force : Recursive Approach 
+Approach : Top - Down  
 
-//User function template for C++
+Time Complexity: O(sum * n), where n is the size of the array.
+Auxiliary Space: O(sum*n), as the size of the 2-D array is sum*n.
 
 class Solution{   
-    private:
-    bool helper(vector<int> &arr,int n ,int sum)
+public:
+
+    bool isSubsetSum(vector<int>arr, int sum){
+int n = arr.size();
+int t[n+1][sum+1];
+for(int i=0;i<n+1;i++)
+{
+    for(int j=0;j<sum+1;j++)
     {
-        if(sum == 0) return true;
-        
-        if(n == 0) return false;
-        
-        if(arr[n-1] <= sum){
-    return helper(arr,n-1,sum-arr[n-1]) || helper(arr,n-1,sum);
+// making first row = false
+        if(i == 0)
+        {
+            t[i][j] = false;
         }
-        
-        else{
-            return helper(arr,n-1,sum);
-        }
-
-    }
-public:
-    bool isSubsetSum(vector<int>arr, int sum){
-    int n = arr.size();
-helper(arr,n,sum);
-    }
-};
-
-Better Approach : Memoization 
-
-//User function template for C++
-
-class Solution{   
-public:
-    bool isSubsetSum(vector<int>arr, int sum){
- int n = arr.size();
-   bool t[n+1][sum+1];
-
- for(int i=0;i<=n;i++)
- {
-     for(int j=0;j<=sum;j++){
-         if(i==0) {
-             t[i][j] = false;
-         }
-         
-        if(j==0){
+        // [0,0] = true
+        if(j==0)
+        {
             t[i][j] = true;
         }
-     }
- }
- 
- for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=sum;j++)
-        {
-            if(arr[i-1] <= j)
-            {
-                t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-            }
-        else{
-            t[i][j] = t[i-1][j];
-        }
-        }
     }
-    return t[n][sum];
+}
+//here i -> n && j -> sum 
+for(int i=1;i<n+1;i++)
+{
+    for(int j=1;j<sum+1;j++)
+    {
+        if(arr[i-1] <= j)
+        {
+            t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+        }
+    
+else{
+    t[i][j] = t[i-1][j];
+}
+    }
+}
+return t[n][sum];
     }
 };
