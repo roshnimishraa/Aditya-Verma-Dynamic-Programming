0fb849bdac55467ce7894d0e72b4120921738fe6
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-11 01:26:38 +0530

    Update Subset Sum Problem.cpp

--- 0-1 Knapsack Problem/Subset Sum Problem.cpp
+++ 0-1 Knapsack Problem/Subset Sum Problem.cpp
@@ -7,41 +7,37 @@ Auxiliary Space: O(sum*n), as the size of the 2-D array is sum*n.
 
 class Solution{   
 public:
-
+int t[101][10005];
     bool isSubsetSum(vector<int>arr, int sum){
-int n = arr.size();
-int t[n+1][sum+1];
-for(int i=0;i<n+1;i++)
-{
-    for(int j=0;j<sum+1;j++)
-    {
-// making first row = false
-        if(i == 0)
-        {
-            t[i][j] = false;
+    //base condition 
+    // if sum == 0 then output will be empty set -> true
+    // if array is empty then no output -> false
+    int n = arr.size();
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i == 0){
+                    t[i][j] = false;
+                }
+                if(j==0){
+                    t[i][j] = true;
+                }
+            }
         }
-        // [0,0] = true
-        if(j==0)
-        {
-            t[i][j] = true;
+        
+    for(int i =1;i<n+1;i++)
+    {
+        for(int j=1;j<sum+1;j++){
+            
+    if(arr[i-1] <= j){
+        t[i][j] = t[i-1][j-arr[i-1]]|| t[i-1][j];
+        
+    }
+    else {
+t[i][j] = t[i-1][j];
+    }
         }
     }
-}
-//here i -> n && j -> sum 
-for(int i=1;i<n+1;i++)
-{
-    for(int j=1;j<sum+1;j++)
-    {
-        if(arr[i-1] <= j)
-        {
-            t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-        }
-    
-else{
-    t[i][j] = t[i-1][j];
-}
-    }
-}
-return t[n][sum];
+        
+        return t[n][sum];
     }
 };
