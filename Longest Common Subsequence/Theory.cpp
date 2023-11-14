44a63b444df474cbdcda619d8e6765b45f76ad00
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-14 10:31:55 +0530

    Create Theory.cpp

---
+++ Longest Common Subsequence/Theory.cpp
@@ -0,0 +1,26 @@
+LCS Variations
+1. Longest Common Substring
+2. Print LCS
+3. Shortest Common Supersequence 
+4. Print SCS 
+5. Minimum number of insertion and deletion a->b
+6. Largest Repeating Subsequence 
+7. Length of Largest Subsequence of [a] which is a substring in [b] 
+8. Subsequence pattern matching 
+9. Count how many times [a] appear as subsequence in [b]
+10. Longest Palindromic Subsequence 
+11. Longest palindromic Substring
+12. Count of Palindromic substring 
+13. Minimum number of deletion in a string to make it a palindrome 
+14. Minimum number of insertion in a string to make it a palindrome 
+
+>>>>>>>Flow of LCS 
+1. Recursive Approach 
+2. Bottom - Up ( Memoization ) 
+3. Top - Down 
+
+>>>>>>>> In Recursive Approach, Remember 3 things 
+1. Base Condition 
+2. Choice Diagram 
+3. Input -> Small 
+
