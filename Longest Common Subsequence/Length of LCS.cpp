9e35db8f7362232e4e23366b7fcc3807557d1416
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-14 11:13:48 +0530

    Create Length of LCS.cpp

---
+++ Longest Common Subsequence/Length of LCS.cpp
@@ -0,0 +1,28 @@
+Problem Link : https://tinyurl.com/28bndgkf
+
+Find : Length of Longest Common Subsequence 
+
+Recursive Approach 
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int n, int m, string s1, string s2)
+    {
+        //base case
+    if(n == 0 || m == 0){
+        return 0;
+    }
+    
+    //choice diagram code
+    if(s1[n-1] == s2[m-1])
+    {
+        return 1+lcs(n-1,m-1,s1,s2);
+    }
+    else{
+        return max(lcs(n,m-1,s1,s2), lcs(n-1,m,s1,s2));
+    }
+    
+    }
+};
