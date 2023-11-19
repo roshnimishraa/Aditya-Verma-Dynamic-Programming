b18df399c339a1663b59b0a9f397af4d6a85cc5f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-19 20:49:46 +0530

    Update Print Longest Common Subsequence.cpp

--- Aditya Verma/Longest Common Subsequence/Print Longest Common Subsequence.cpp
+++ Aditya Verma/Longest Common Subsequence/Print Longest Common Subsequence.cpp
@@ -1,51 +1,64 @@
 Problem Link : https://tinyurl.com/yqgk8vp3
+
 Approach : Longest Common Subseuences 
 
-int t[1002][1002];
-int lcs(int n,int m,string &s1,string &s2){
-//initialization 
-for(int i=0;i<m+1;i++)
-{
-	for(int j=0;j<n+1;j++){
-	if(i==0 || j==0){
-	t[i][j] = 0;
-	}
 
-	}
+class Solution {
+public:
+int t[1002][1002];
+int lcs(string x,string y,int m,int n){
+// initialization 
+for(int i=0;i<m+1;i++){
+    for(int j=0;j<n+1;j++){
+        if(i==0 || j==0){
+            t[i][j] = 0;
+        }
+    }
 }
 
 for(int i=1;i<m+1;i++){
-	for(int j=1;j<n+1;j++){
-	if(s1[i-1] == s2[j-1]){
-t[i][j] = 1 + t[i-1][j-1];
-	}
+    for(int j=1;j<n+1;j++){
+    if(x[i-1] == y[j-1])
+    {
+        t[i][j] = 1 + t[i-1][j-1];
+    }
 else{
-	t[i][j] = max(t[i-1][j], t[i][j-1]);
+    t[i][j] = max(t[i-1][j], t[i][j-1]);
 }
-	}
+}
 }
 return t[m][n];
 }
-string findLCS(int n, int m,string &s1, string &s2)
-{
-int result = lcs(n,m,s1,s2);
-int i = m,j=n;
-string s = "";
+    string shortestCommonSupersequence(string str1, string str2) {
+   int m = str1.length();
+   int n = str2.length();
+      int ans = lcs(str1,str2,m,n);
+   int i=m, j = n;
+   string s = "" ; //scs print 
+
 while(i>0 && j>0){
-	if(s1[i-1] ==s2[j-1]){
-		s.push_back(s1[i-1]);
-		i--;
-		j--;
-	}
-	else{
-if(t[i][j-1] > t[i-1][j]){
-	j--;
+    if(str1[i-1] == str2[j-1])
+    {
+        // store common 
+        s.push_back(str1[i-1]);
+        i--;
+        j--;
+    }
+    // str1 != str2 
+    else{
+    // store and assign max 
+        if(t[i][j-1] > t[i-1][j])
+        {
+            j--;
+        }
+        else{
+            i--;
+        }
+    }
 }
-else {
-	i--;
-}
-	}
-}
+// since iteration start from t[m][n] then the ans is in   
+//reverse order therefore reverse to get the original ans
 reverse(s.begin(),s.end());
 return s;
-}
+    }
+};
