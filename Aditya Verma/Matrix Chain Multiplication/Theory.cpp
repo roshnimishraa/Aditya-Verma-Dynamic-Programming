5ddc5588f6b94055712925b71079c6beb653005a
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-20 23:43:50 +0530

    Create Theory.cpp

---
+++ Aditya Verma/Matrix Chain Multiplication/Theory.cpp
@@ -0,0 +1,8 @@
+Matrix Chain Multiplication 
+1. MCM 
+2. Printing MCM
+3. Printing expression to true/ Boolean Parenthensization 
+4. Min / Max value of an expression 
+5. Palindrome Partitioning 
+6. Scramble String 
+7. Egg Dropping Problem 
